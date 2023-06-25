9aa0789efe6ab7a8bf22c91fda5092ddb0a33417
Tobias Thaden <hello@tobiasthaden.com>
2023-06-25 13:34:35 +0200

    Handle validation messages

--- resources/js/Shared/ResourceFields.jsx
+++ resources/js/Shared/ResourceFields.jsx
@@ -1,6 +1,7 @@
 import Form, {Field, Label} from "~/Components/Form";
+import {Error} from "../Components/Form";
 
-export default function ResourceFields({fields = {}, onChange}) {
+export default function ResourceFields({fields = {}, errors = {}, onChange}) {
     return (
         <Form>
             {Object.entries(fields).map(([name, {label, required, ...props}]) => (
@@ -12,6 +13,7 @@ export default function ResourceFields({fields = {}, onChange}) {
                     </div>
                     <div className="w-full">
                         <Field id={name} required={required} onChange={value => onChange(name, value)} {...props} />
+                        {!!errors[name] && <Error>{errors[name]}</Error>}
                     </div>
                 </Form.Section>
             ))}
