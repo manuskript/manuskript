bb165e9908b26dc0f3a5966d933f22bc15f8a3cd
Tobias Thaden <hello@tobiasthaden.com>
2024-03-03 19:33:31 +0100

    Prefix resources routes

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,10 @@ use Manuskript\Http\Controllers\ResourcesController;
 Route::name('manuskript.')->group(function () {
     Route::get('/', DashboardController::class)->name('dashboard');
 
-    Route::get('{resource}', [ResourcesController::class, 'index'])->name('resources.index');
-    Route::post('{resource}', [ResourcesController::class, 'store'])->name('resources.store');
-    Route::get('{resource}/new', [ResourcesController::class, 'create'])->name('resources.create');
-    Route::get('{resource}/{model}', [ResourcesController::class, 'show'])->name('resources.show');
-    Route::get('{resource}/{model}/edit', [ResourcesController::class, 'edit'])->name('resources.edit');
-    Route::patch('{resource}/{model}', [ResourcesController::class, 'update'])->name('resources.update');
+    Route::get('resources/{resource}', [ResourcesController::class, 'index'])->name('resources.index');
+    Route::post('resources/{resource}', [ResourcesController::class, 'store'])->name('resources.store');
+    Route::get('resources/{resource}/new', [ResourcesController::class, 'create'])->name('resources.create');
+    Route::get('resources/{resource}/{model}', [ResourcesController::class, 'show'])->name('resources.show');
+    Route::get('resources/{resource}/{model}/edit', [ResourcesController::class, 'edit'])->name('resources.edit');
+    Route::patch('resources/{resource}/{model}', [ResourcesController::class, 'update'])->name('resources.update');
 });
