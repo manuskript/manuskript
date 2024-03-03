fd53d634fbc5fb733d41e910454e282cfa75fa50
Tobias Thaden <hello@tobiasthaden.com>
2024-03-03 22:03:41 +0100

    Create filesystem builder
    to allow fetching files and directories from the configured local or cloud filesystems

---
+++ src/Filesystem/Builder.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Manuskript\Filesystem;
+
+use Illuminate\Contracts\Config\Repository as Config;
+use Illuminate\Contracts\Filesystem\Factory;
+use Illuminate\Contracts\Filesystem\Filesystem;
+use Manuskript\Exceptions\FileNotFoundHttpException;
+use Manuskript\Support\Str;
+
+class Builder
+{
+    private Filesystem $filesystem;
+
+    public function __construct(
+        private readonly Factory $factory,
+        private readonly Config $config,
+    ) {
+        $this->filesystem = $factory->disk(
+            $config->get('manuskript.filesystem.disk')
+        );
+    }
+
+    public function directories(?string $path)
+    {
+        $path = $this->getValidatedPath($path);
+
+        return $this->transformCollection(
+            $this->filesystem->directories($path)
+        );
+    }
+
+    public function files(?string $path)
+    {
+        $path = $this->getValidatedPath($path);
+
+        return $this->transformCollection(
+            $this->filesystem->files($path)
+        );
+    }
+
+    private function getValidatedPath(?string $path): ?string
+    {
+        $entryPath = $this->getEntryPath();
+
+        if(empty($path)) {
+            return $entryPath;
+        }
+
+        $path = trim($path);
+
+        if(empty($entryPath) || $path === $entryPath) {
+            return $path;
+        }
+
+        if(Str::startsWith($path, $entryPath . '/')) {
+            return $path;
+        }
+
+        throw new FileNotFoundHttpException();
+    }
+
+    private function getEntryPath(): ?string
+    {
+        $entryPath = $this->config->get('manuskript.filesystem.folder');
+
+        if(! is_null($entryPath)) {
+            $entryPath = trim($entryPath);
+        }
+
+        return $entryPath;
+    }
+
+    private function transformCollection(array|string $items): Collection
+    {
+        $items = is_array($items) ? $items : [$items];
+
+        return new Collection(
+            array_map(static fn(string $path) => static::transform($path), $items)
+        );
+    }
+
+    private static function transform(string $path): Resource
+    {
+        return new Resource($path);
+    }
+}
---
+++ src/Filesystem/Collection.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Manuskript\Filesystem;
+
+use Manuskript\Support\Collection as BaseCollection;
+
+class Collection extends BaseCollection
+{
+    //
+}
