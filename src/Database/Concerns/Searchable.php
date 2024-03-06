61b74e85dad2bd88ee45741292b953290c48490f
Tobias Thaden <hello@tobiasthaden.com>
2024-03-06 22:52:38 +0100

    Call query methods from the resource

--- src/Database/Concerns/Searchable.php
+++ src/Database/Concerns/Searchable.php
@@ -1,21 +1,21 @@
 <?php
 
-namespace Manuskript\Concerns;
+namespace Manuskript\Database\Concerns;
 
 use Closure;
 
 trait Searchable
 {
-    protected static ?Closure $search;
+    protected static ?Closure $search = null;
 
     public static function searchUsing(Closure $callback): void
     {
         static::$search = $callback;
     }
 
-    public static function search()
+    public static function search($query, $term): void
     {
-        return static::$search;
+        (static::$search ?: fn () => null)($query, $term);
     }
 
     public static function searchable(): bool
